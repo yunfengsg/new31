309321b85e32a602eb61dfa6fa31dfbb0aa7ec43
yunfengsg <yunfengsg@gmail.com>
2025-02-04 22:05:08 +0800

    activity2

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+resource "aws_s3_bucket" {
+  bucket = "yyf-workflow"
+  force_destory = true
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce8-tfstate"
+    key    = "yyf.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
